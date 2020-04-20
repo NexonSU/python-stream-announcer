b70c4e9c9d6f0f96539cd05286637bdda8d8d2bd
NexonSU <nexonrievsky@gmail.com>
2020-04-20 18:07:57 +0500

    Change temp file storage to something like pickle

--- twitch.py
+++ twitch.py
@@ -7,31 +7,34 @@ import time
 import datetime
 import requests
 import logging
+import pickle
 
-temp_folder_name = __file__.rsplit('.', 1)[0]
+script_name = __file__.rsplit('.', 1)[0]
 
-logging.basicConfig(filename=f"{temp_folder_name}.log", level=config.loglevel, format=config.logformat, datefmt=config.logdatefmt)
+#log init
+logging.basicConfig(filename=f"{script_name}.log", level=config.loglevel, format=config.logformat, datefmt=config.logdatefmt)
 
-#checking temp files
-if not os.path.exists(temp_folder_name): os.makedirs(temp_folder_name)
-tempfiles = ['game', 'msg', 'status', 'stream', 'viewers']
-for file in tempfiles: 
-	if not os.path.exists(f'{temp_folder_name}/{file}'): 
-		os.mknod(f'{temp_folder_name}/{file}')
+#checking pickle file
+if not os.path.exists(f'{script_name}.pkl'):
+	storage = {'game': 'None', 'msg': '0', 'status': 'None', 'stream': 'offline', 'viewers': '0'} 
+	pickle.dump(storage, open(f'{script_name}.pkl', 'wb'))
+
+#loading variables from pickle file
+storage = pickle.load(open(f'{script_name}.pkl', 'rb'))
 
 #checking status and game changes
 channel = requests.get(f'https://api.twitch.tv/kraken/channels/{config.twitch_channelid}', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)', 'Client-ID': config.twitch_client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}).json()
 status = channel['status']
 game = channel['game']
 
-if open(f'{temp_folder_name}/status', 'r').read() != status:
-	open(f'{temp_folder_name}/status', 'wt', encoding='utf-8').write(status)
+if storage['status'] != status:
+	storage['status'] = status
 	message = f'Статус изменен на "{status}".'
 	logging.info(message)
 	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
 
-if open(f'{temp_folder_name}/game', 'r').read() != game:
-	open(f'{temp_folder_name}/game', 'wt', encoding='utf-8').write(game)
+if storage['game'] != game:
+	storage['game'] = game
 	message = f'Категория изменена на "{game}".'
 	logging.info(message)
 	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
@@ -46,33 +49,36 @@ if stream.get("stream") != None:
 	thumbnail = stream['preview']['large']+'?'+str(time.time())
 	logging.info('Stream online')
 	logging.info(f'Viewers: {viewers}')
-	if open(f'{temp_folder_name}/stream', 'r').read() != 'online':
+	if storage['stream'] != 'online':
 		#posting stream announce in telegram
-		open(f'{temp_folder_name}/stream', 'wt', encoding='utf-8').write('online')
-		open(f'{temp_folder_name}/viewers', 'wt', encoding='utf-8').write('0')
+		storage['stream'] = 'online'
+		storage['viewers'] = 0
 		message = f'Стрим "{status}" начался.\nКатегория: {game}.\nhttps://twitch.tv/{config.twitch_channelname}'
 		logging.info(message)
 		msg = requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendPhoto', data={'chat_id': config.telegram_chat, 'photo': thumbnail, 'caption': message}).json()
-		open(f'{temp_folder_name}/msg', 'wt', encoding='utf-8').write(str(msg['result']['message_id']))
+		storage['msg'] = msg['result']['message_id']
 	else:
 		#updating stream information in telegram
-		msg = open(f'{temp_folder_name}/msg', 'r').read();
+		msg = storage['msg']
 		caption = f'Стрим: {status}.\nКатегория: {game}.\nОнлайн: {viewers}.\nhttps://twitch.tv/{config.twitch_channelname}'
 		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/editMessageMedia', data={'chat_id': config.telegram_chat, 'message_id': msg, 'media': json.dumps({'type': 'photo', 'media': thumbnail, 'caption': caption})})
-	if int(open(f'{temp_folder_name}/viewers', 'r').read()) < int(viewers):
+	if int(storage['viewers']) < int(viewers):
 		#updating viewers count
-		open(f'{temp_folder_name}/viewers', 'wt', encoding='utf-8').write(viewers)
+		storage['viewers'] = viewers
 else:
 	logging.info('Stream offline')
-	if open(f'{temp_folder_name}/stream', 'r').read() != 'offline':
+	if storage['stream'] != 'offline':
 		#posting end of stream in telegram
-		open(f'{temp_folder_name}/stream', 'wt', encoding='utf-8').write('offline')
-		viewers = open(f'{temp_folder_name}/viewers', 'r').read();
-		status = open(f'{temp_folder_name}/status', 'r').read();
-		msg = open(f'{temp_folder_name}/msg', 'r').read();
+		storage['stream'] = 'offline'
+		viewers = storage['viewers']
+		status = storage['status']
+		msg = storage['msg']
 		message = f'Стрим "{status}" закончился.\nМаксимум зрителей за стрим: {viewers}'
 		logging.info(message)
 		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'reply_to_message_id': msg, 'disable_notification': True})
 
 logging.info(f'Status: {status}')
 logging.info(f'Category: {game}')
+
+#store variables to pickle file
+pickle.dump(storage, open(f'{script_name}.pkl', 'wb'))
--- youtube.py
+++ youtube.py
@@ -5,22 +5,27 @@ import os
 import sys
 import time
 import requests
-import logging
 from bs4 import BeautifulSoup
+import logging
+import pickle
  
-temp_folder_name = __file__.rsplit('.', 1)[0]
+script_name = __file__.rsplit('.', 1)[0]
 
-logging.basicConfig(filename=f"{temp_folder_name}.log", level=config.loglevel, format=config.logformat, datefmt=config.logdatefmt)
+#log init
+logging.basicConfig(filename=f"{script_name}.log", level=config.loglevel, format=config.logformat, datefmt=config.logdatefmt)
 
-#checking temp files
-if not os.path.exists(temp_folder_name): os.makedirs(temp_folder_name)
-tempfiles = ['game', 'msg', 'status', 'stream', 'viewers']
-for file in tempfiles: 
-	if not os.path.exists(f'{temp_folder_name}/{file}'): 
-		os.mknod(f'{temp_folder_name}/{file}')
-if not os.path.exists(f'{temp_folder_name}/categorylist'):
-	categorylist = requests.get('https://www.googleapis.com/youtube/v3/videoCategories', params={'part': 'snippet', 'regionCode': 'ru', 'hl': 'ru_RU', 'key': config.youtube_api_key}).text
-	open(f'{temp_folder_name}/categorylist', 'wt', encoding='utf-8').write(categorylist)
+#checking pickle file
+if not os.path.exists(f'{script_name}.pkl'):
+	storage = {'category': 'None', 'msg': '0', 'status': 'None', 'stream': 'offline', 'viewers': '0', 'categorylist_update_date': 0, 'categorylist': '', 'stream-url': '', 'thumbnail': ''} 
+	pickle.dump(storage, open(f'{script_name}.pkl', 'wb'))
+
+#loading variables from pickle file
+storage = pickle.load(open(f'{script_name}.pkl', 'rb'))
+
+#update categorylist
+if int(time.time())-int(storage['categorylist_update_date']) > 2630000:
+	storage['categorylist'] = requests.get('https://www.googleapis.com/youtube/v3/videoCategories', params={'part': 'snippet', 'regionCode': 'ru', 'hl': 'ru_RU', 'key': config.youtube_api_key}).json()
+	storage['categorylist_update_date'] = int(time.time())
 
 #checking for videoid on stream page 
 stream = requests.get('https://www.youtube.com/embed/live_stream', params={'channel': config.youtube_channelid}).content
@@ -30,20 +35,20 @@ videoid = soup.head.find_all('link', rel="canonical")[0]['href'].split('watch?v=
 #getting stream information
 stream = requests.get('https://www.googleapis.com/youtube/v3/videos', params={'part': 'snippet,liveStreamingDetails', 'id': videoid, 'key': config.youtube_api_key}).json().get("items")[0]
 
-#checking status and game changes
-categorylist = json.loads(open(f'{temp_folder_name}/categorylist', 'r').read());
+#checking status and category changes
+categorylist = storage['categorylist']
 category = [subd['snippet']['title'] for subd in categorylist['items'] if subd['id'] == stream['snippet']['categoryId']][0]
 status = stream['snippet']['title']
 thumbnail = 'https://zavtrastream.nexon.su/thumbnail.jpg?'+str(time.time())
 
-if open(f'{temp_folder_name}/status', 'r').read() != status:
-	open(f'{temp_folder_name}/status', 'wt', encoding='utf-8').write(status)
+if storage['status'] != status:
+	storage['status'] = status
 	message = f'Статус изменен на "{status}".'
 	logging.info(message)
 	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
 
-if open(f'{temp_folder_name}/game', 'r').read() != category:
-	open(f'{temp_folder_name}/game', 'wt', encoding='utf-8').write(category)
+if storage['category'] != category:
+	storage['category'] = category
 	message = f'Категория изменена на "{category}".'
 	logging.info(message)
 	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
@@ -53,41 +58,44 @@ if 'concurrentViewers' in stream['liveStreamingDetails']:
 	logging.info('Stream online')
 	viewers = str(stream['liveStreamingDetails']['concurrentViewers'])
 	logging.info(f'Viewers: {viewers}')
-	if open(f'{temp_folder_name}/stream', 'r').read() != 'online':
+	if storage['stream'] != 'online':
 		#announcing stream in telegram
-		open(f'{temp_folder_name}/stream', 'wt', encoding='utf-8').write('online')
-		open(f'{temp_folder_name}/viewers', 'wt', encoding='utf-8').write('0')
+		storage['stream'] = 'online'
+		storage['viewers'] = 0
 		message = f'Стрим "{status}" начался.\nКатегория: {category}.\nhttps://www.youtube.com/watch?v={videoid}'
 		logging.info(message)
-		os.system(f'youtube-dl --get-url "https://www.youtube.com/watch?v={videoid}" > {temp_folder_name}/stream-url')
+		os.system(f'youtube-dl --get-url "https://www.youtube.com/watch?v={videoid}" > {script_name}/stream-url')
 		time.sleep(1)
-		os.system(f'ffmpeg -i $(cat {temp_folder_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {temp_folder_name}/thumbnail.jpg')
+		os.system(f'ffmpeg -i $(cat {script_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {script_name}/thumbnail.jpg')
 		time.sleep(3)
 		msg = requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendPhoto', data={'chat_id': config.telegram_chat, 'photo': thumbnail, 'caption': message}).json()
-		open(f'{temp_folder_name}/msg', 'wt', encoding='utf-8').write(str(msg['result']['message_id']))
+		storage['msg'] = msg['result']['message_id']
 	else:
 		#updating stream information in telegram
-		msg = open(f'{temp_folder_name}/msg', 'r').read();
+		msg = storage['msg']
 		caption = f'Стрим: {status}.\nКатегория: {category}.\nОнлайн: {viewers}.\nhttps://www.youtube.com/watch?v={videoid}'
-		os.system(f'youtube-dl --get-url "https://www.youtube.com/watch?v={videoid}" > {temp_folder_name}/stream-url')
+		os.system(f'youtube-dl --get-url "https://www.youtube.com/watch?v={videoid}" > {script_name}/stream-url')
 		time.sleep(1)
-		os.system(f'ffmpeg -i $(cat {temp_folder_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {temp_folder_name}/thumbnail.jpg')
+		os.system(f'ffmpeg -i $(cat {script_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {script_name}/thumbnail.jpg')
 		time.sleep(3)
 		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/editMessageMedia', data={'chat_id': config.telegram_chat, 'message_id': msg, 'media': json.dumps({'type': 'photo', 'media': thumbnail, 'caption': caption})})
-	if int(open(f'{temp_folder_name}/viewers', 'r').read()) < int(viewers):
+	if int(storage['viewers']) < int(viewers):
 		#updating viewers count
-		open(f'{temp_folder_name}/viewers', 'wt', encoding='utf-8').write(str(viewers))
+		storage['viewers'] = viewers
 else:
 	logging.info('Stream offline')
-	if open(f'{temp_folder_name}/stream', 'r').read() != 'offline':
+	if storage['stream'] != 'offline':
 		#posting end of stream in telegram
-		open(f'{temp_folder_name}/stream', 'wt', encoding='utf-8').write('offline')
-		viewers = open(f'{temp_folder_name}/viewers', 'r').read();
-		status = open(f'{temp_folder_name}/status', 'r').read();
-		msg = open(f'{temp_folder_name}/msg', 'r').read();
+		storage['stream'] = 'offline'
+		viewers = storage['viewers']
+		status = storage['status']
+		msg = storage['msg']
 		message = f'Стрим "{status}" закончился.\nМаксимум зрителей за стрим: {viewers}.'
 		logging.info(message)
 		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'reply_to_message_id': msg, 'disable_notification': True})
 
 logging.info(f'Status: {status}')
 logging.info(f'Category: {category}')
+
+#store variables to pickle file
+pickle.dump(storage, open(f'{script_name}.pkl', 'wb'))
