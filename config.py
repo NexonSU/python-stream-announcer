0d444a02f764978d483f78c4bafbd179b2ab47ba
NexonSU <nexonrievsky@gmail.com>
2020-04-20 15:28:53 +0500

    Move keys and IDs to config file

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#telegram
+telegram_token = 'example:example' #to get token, create bot via @botfather at telegram https://t.me/BotFather
+telegram_chat = '@example' #your telegram channel
+
+#twitch
+twitch_client_id = 'example' #to get Client ID, visit https://dev.twitch.tv/docs/authentication#registration
+twitch_channelid = 'example' #to get your channel ID, you need to make API call, but you can just open your Twitch page and lookup for some API requests from DevTools > Network, for example https://api.twitch.tv/helix/streams?user_id=<your ID>
+twitch_channelname = 'example' #for link in messages
+
+#youtube
+youtube_api_key = 'example' #to get api key, visit https://developers.google.com/youtube/v3/getting-started
+youtube_channelid = 'example' #your youtube channel ID. To get channel ID, just open "Your channel" from user panel dock and copy channel ID from address bar.
--- youtube.py
+++ youtube.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import config
 import json
 import os
 import sys
@@ -7,10 +9,6 @@ import requests
 from bs4 import BeautifulSoup
 
 temp_folder_name = __file__.rsplit('.', 1)[0]
-youtube_api_key = 'example' #to get api key, visit https://developers.google.com/youtube/v3/getting-started
-telegram_token = 'example:example' #to get token, create bot via @botfather at telegram https://t.me/BotFather
-telegram_chat = '@example' #your telegram channel
-youtube_channelid = 'example' #your youtube channel ID. To get channel ID, just open "Your channel" from user panel dock and copy channel ID from address bar.
 
 #checking temp files
 if not os.path.exists(temp_folder_name): os.makedirs(temp_folder_name)
@@ -19,16 +17,16 @@ for file in tempfiles:
 	if not os.path.exists(f'{temp_folder_name}/{file}'): 
 		os.mknod(f'{temp_folder_name}/{file}')
 if not os.path.exists(f'{temp_folder_name}/categorylist'):
-	categorylist = requests.get('https://www.googleapis.com/youtube/v3/videoCategories', params={'part': 'snippet', 'regionCode': 'ru', 'hl': 'ru_RU', 'key': youtube_api_key}).text
+	categorylist = requests.get('https://www.googleapis.com/youtube/v3/videoCategories', params={'part': 'snippet', 'regionCode': 'ru', 'hl': 'ru_RU', 'key': config.youtube_api_key}).text
 	open(f'{temp_folder_name}/categorylist', 'wt', encoding='utf-8').write(categorylist)
 
 #checking for videoid on stream page 
-stream = requests.get('https://www.youtube.com/embed/live_stream', params={'channel': youtube_channelid}).content
+stream = requests.get('https://www.youtube.com/embed/live_stream', params={'channel': config.youtube_channelid}).content
 soup = BeautifulSoup(stream, features="html.parser")
 videoid = soup.head.find_all('link', rel="canonical")[0]['href'].split('watch?v=')[1]
 
 #getting stream information
-stream = requests.get('https://www.googleapis.com/youtube/v3/videos', params={'part': 'snippet,liveStreamingDetails', 'id': videoid, 'key': youtube_api_key}).json().get("items")[0]
+stream = requests.get('https://www.googleapis.com/youtube/v3/videos', params={'part': 'snippet,liveStreamingDetails', 'id': videoid, 'key': config.youtube_api_key}).json().get("items")[0]
 
 #checking status and game changes
 categorylist = json.loads(open(f'{temp_folder_name}/categorylist', 'r').read());
@@ -39,12 +37,12 @@ thumbnail = 'https://zavtrastream.nexon.su/thumbnail.jpg?'+str(time.time())
 if open(f'{temp_folder_name}/status', 'r').read() != status:
 	open(f'{temp_folder_name}/status', 'wt', encoding='utf-8').write(status)
 	message = f'Статус изменен на "{status}".'
-	requests.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data={'chat_id': telegram_chat, 'text': message, 'disable_notification': True})
+	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
 
 if open(f'{temp_folder_name}/game', 'r').read() != category:
 	open(f'{temp_folder_name}/game', 'wt', encoding='utf-8').write(category)
 	message = f'Категория изменена на "{category}".'
-	requests.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data={'chat_id': telegram_chat, 'text': message, 'disable_notification': True})
+	requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'disable_notification': True})
 
 #checking stream online status
 if 'concurrentViewers' in stream['liveStreamingDetails']:
@@ -58,7 +56,7 @@ if 'concurrentViewers' in stream['liveStreamingDetails']:
 		time.sleep(1)
 		os.system(f'ffmpeg -i $(cat {temp_folder_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {temp_folder_name}/thumbnail.jpg')
 		time.sleep(3)
-		msg = requests.post(f'https://api.telegram.org/bot{telegram_token}/sendPhoto', data={'chat_id': telegram_chat, 'photo': thumbnail, 'caption': message}).json()
+		msg = requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendPhoto', data={'chat_id': config.telegram_chat, 'photo': thumbnail, 'caption': message}).json()
 		open(f'{temp_folder_name}/msg', 'wt', encoding='utf-8').write(str(msg['result']['message_id']))
 	else:
 		#updating stream information in telegram
@@ -68,7 +66,7 @@ if 'concurrentViewers' in stream['liveStreamingDetails']:
 		time.sleep(1)
 		os.system(f'ffmpeg -i $(cat {temp_folder_name}/stream-url) -f image2 -frames:v 1 -y -v 0 {temp_folder_name}/thumbnail.jpg')
 		time.sleep(3)
-		requests.post(f'https://api.telegram.org/bot{telegram_token}/editMessageMedia', data={'chat_id': telegram_chat, 'message_id': msg, 'media': json.dumps({'type': 'photo', 'media': thumbnail, 'caption': caption})})
+		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/editMessageMedia', data={'chat_id': config.telegram_chat, 'message_id': msg, 'media': json.dumps({'type': 'photo', 'media': thumbnail, 'caption': caption})})
 	if int(open(f'{temp_folder_name}/viewers', 'r').read()) < int(viewers):
 		#updating viewers count
 		open(f'{temp_folder_name}/viewers', 'wt', encoding='utf-8').write(str(viewers))
@@ -80,4 +78,4 @@ else:
 		status = open(f'{temp_folder_name}/status', 'r').read();
 		msg = open(f'{temp_folder_name}/msg', 'r').read();
 		message = f'Стрим "{status}" закончился.\nМаксимум зрителей за стрим: {viewers}.'
-		requests.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data={'chat_id': telegram_chat, 'text': message, 'reply_to_message_id': msg, 'disable_notification': True})
+		requests.post(f'https://api.telegram.org/bot{config.telegram_token}/sendMessage', data={'chat_id': config.telegram_chat, 'text': message, 'reply_to_message_id': msg, 'disable_notification': True})
